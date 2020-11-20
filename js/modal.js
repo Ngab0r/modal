44a976c8361239dc538de76dbdb98e204fb42132
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2020-11-20 17:24:51 +0100

    project done

---
+++ js/modal.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const modalWindow = document.querySelector('.modal');
+const modalButton = document.querySelector('#modalButton');
+const main = document.querySelector('.main');
+const closeX = document.querySelectorAll('.closeX');
+
+(function addModalButtonEvent() {
+    modalButton.addEventListener('click', function() {
+        if (modalWindow.classList.contains('hide')) {
+            showModalWindow();
+        } else {
+            hideModalWindow();
+        }
+    })
+})()
+
+function addCloseXEvent() {
+    for (let i = 0; i < closeX.length; i++) {
+        closeX[i].addEventListener('click', () => hideModalWindow());
+    }
+}
+addCloseXEvent();
+
+function showModalWindow() {
+    modalWindow.classList.remove('hide');
+    modalWindow.classList.add('show');
+    main.classList.remove('noOverlay');
+    main.classList.add('overlay');
+    window.onclick = function(event) {
+        if (event.target == main) {
+            hideModalWindow();
+        }
+    }
+}
+
+function hideModalWindow() {
+    modalWindow.classList.remove('show');
+    modalWindow.classList.add('hide');
+    main.classList.remove('overlay');
+    main.classList.add('noOverlay');
+
+}
